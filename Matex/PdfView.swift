687b577c02ccc1cce959a989b9717db6ad8afdca
Sahil Sharma <timothygreen573@gmail.com>
2023-04-09 14:16:52 +0530

    removed exposed api key
    - removed exposed api key to environment variable

--- Matex/PdfView.swift
+++ Matex/PdfView.swift
@@ -9,8 +9,37 @@ import SwiftUI
 
 struct PdfView: View {
     var body: some View {
-        VStack{
+        ZStack(alignment: .bottomTrailing) {
             PdfKitView(pdfUrl: "https://www.africau.edu/images/default/sample.pdf")
+            HStack{
+                Button {
+                    //                Start Download
+                    //                isButtonClicked = true
+                } label: {
+                    Label("Download", systemImage: "square.and.arrow.down")
+                        .padding(10)
+                }
+                .background(Color(red: 242 / 255, green: 242 / 255, blue: 242 / 255))
+                .clipShape(Rectangle())
+                .cornerRadius(10)
+                .padding(20)
+                .shadow(color: Color.black.opacity(0.2),radius: 5)
+                
+                Spacer()
+                
+                Button {
+                    //                Start Download
+                    //                isButtonClicked = true
+                } label: {
+                    Label("Render", systemImage: "highlighter")
+                        .padding(10)
+                }
+                .background(Color(red: 242 / 255, green: 242 / 255, blue: 242 / 255))
+                .clipShape(Rectangle())
+                .cornerRadius(10)
+                .padding(20)
+                .shadow(color: Color.black.opacity(0.2),radius: 5)
+            }
         }
     }
 }
